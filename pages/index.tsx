fe9449649ad6200505b9f9f96427a17c343fc6db
tulsidas-ev <tulsi.das@ev-smartmoney.com>
2022-12-05 19:17:03 +0100

    display some of the airports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Layout from "../components/layout";
 import useApiData from "../hooks/use-api-data";
@@ -10,6 +10,48 @@ const Page: NextPage = () => {
   const [query, setQuery] = useState<string>("");
 
   const airports = useApiData<Airport[]>(`/api/airports/${query}`, [], [query]);
+
+  const [cluster, setCluster] = useState({
+    start: 0,
+    end: 40,
+  });
+  const listRef = useRef<HTMLElement>();
+
+  useEffect(() => {
+    const updateCluster = () => {
+      const SWAP_SIZE = 20;
+
+      if (
+        listRef.current.firstElementChild.getBoundingClientRect().bottom > 0
+      ) {
+        const next = cluster.start < SWAP_SIZE ? cluster.start : SWAP_SIZE;
+        if (next > 0) {
+          setCluster((cur) => ({
+            start: cur.start - next,
+            end: cur.end - next,
+          }));
+        }
+      }
+      if (
+        listRef.current.lastElementChild.getBoundingClientRect().top <
+        window.innerHeight
+      ) {
+        const remaining = airports.length - cluster.end;
+        const next = remaining > SWAP_SIZE ? SWAP_SIZE : remaining;
+        if (next > 0) {
+          setCluster((cur) => ({
+            start: cur.start + next,
+            end: cur.end + next,
+          }));
+        }
+      }
+    };
+    document.addEventListener("scroll", updateCluster);
+
+    return () => {
+      document.removeEventListener("scroll", updateCluster);
+    };
+  }, [airports, cluster]);
 
   return (
     <Layout>
@@ -33,8 +75,8 @@ const Page: NextPage = () => {
         </span>
       </h2>
 
-      <div className="grid sm:grid-cols-2 gap-5 mt-5">
-        {airports.map((airport) => (
+      <div ref={listRef} className="grid sm:grid-cols-2 gap-5 mt-5">
+        {airports.slice(cluster.start, cluster.end).map((airport) => (
           <Link
             className="flex flex-col justify-between gap-2 p-5 text-gray-800 border border-gray-200 rounded-lg shadow-sm hover:border-blue-600 focus:border-blue-600 focus:ring focus:ring-blue-200 focus:outline-none"
             href={`/airports/${airport.iata.toLowerCase()}`}
